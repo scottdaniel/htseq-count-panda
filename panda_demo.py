dee1fded9e3c8dfead8addab67f910ffd5f38837
Scott Daniel <scottdaniel25@gmail.com>
2016-10-01 15:24:58 -0700

    sweet made it through tutorial

--- panda_demo.py
+++ panda_demo.py
@@ -58,3 +58,79 @@ df['sum'] = df.apply(func=sum,axis=1)
 df
 df[df['sum']>7]
 
+df
+df1 = df.reindex(index=dates[0:4],columns=list(df.columns) + ['E'])
+df1
+df1.loc[dates[0]:dates[1],'E'] = 1
+df1.dropna(how='any')
+df1.fillna(value=5)
+pd.isnull(df1)
+df.mean()
+df.mean(1)
+s = pd.Series([1,3,5,np.nan,6,8], index=dates).shift(2)
+s
+df.sub(s,axis='index')
+
+df.apply(np.cumsum)
+df.apply(lambda x: x.max() - x.min())
+
+s = pd.Series(np.random.randint(0, 7, size=10))
+s
+s.value_counts()
+s = pd.Series(['A', 'B', 'C', 'Aaba', 'Baca', np.nan, 'CABA', 'dog', 'cat'])
+s
+s.str.lower()
+df = pd.DataFrame(np.random.randn(10, 4))
+df
+pieces = [df[:3], df[3:7], df[7:]]
+pieces
+pieces[2]
+pd.concat(pieces)
+
+left = pd.DataFrame({'key': ['foo', 'foo','bar'], 'lval': [1, 2, 3]})
+left
+right = pd.DataFrame({'key': ['foo', 'foo','baz'], 'rval': [4, 5, 6]})
+right
+pd.merge(left,right,on='key',how='inner')
+pd.merge(left,right,on='key',how='right')
+pd.merge(left,right,on='key',how='left')
+pd.merge(left,right,on='key',how='outer')
+
+df = pd.DataFrame(np.random.randn(8, 4), columns=['A','B','C','D'])
+df
+s = df.iloc[3]
+s
+df.append(s,ignore_index=True)
+
+df = pd.DataFrame({'A' : ['foo', 'bar', 'foo', 'bar',
+   ....:                           'foo', 'bar', 'foo', 'foo'],
+   ....:                    'B' : ['one', 'one', 'two', 'three',
+   ....:                           'two', 'two', 'one', 'three'],
+   ....:                    'C' : np.random.randn(8),
+   ....:                    'D' : np.random.randn(8)})
+df
+df.groupby('A').sum()
+df.groupby(['A','B']).sum()
+
+tuples = list(zip(*[['bar', 'bar', 'baz', 'baz',
+   ....:                      'foo', 'foo', 'qux', 'qux'],
+   ....:                     ['one', 'two', 'one', 'two',
+   ....:                      'one', 'two', 'one', 'two']]))
+tuples
+index = pd.MultiIndex.from_tuples(tuples, names=['first', 'second'])
+index
+df = pd.DataFrame(np.random.randn(8, 2), index=index, columns=['A', 'B'])
+df
+df2 = df[:4]
+df2
+stacked = df2.stack()
+stacked
+stacked.unstack(0)
+
+df = pd.DataFrame({'A' : ['one', 'one', 'two', 'three'] * 3,
+   .....:                    'B' : ['A', 'B', 'C'] * 4,
+   .....:                    'C' : ['foo', 'foo', 'foo', 'bar', 'bar', 'bar'] * 2,
+   .....:                    'D' : np.random.randn(12),
+   .....:                    'E' : np.random.randn(12)})
+df
+pd.pivot_table(df, values='D', index=['A', 'B'], columns=['C'])
--- playing with htseq-count output.py
+++ playing with htseq-count output.py
@@ -1,9 +1,21 @@
 import os
-os.chdir("Desktop")
+os.chdir("/Users/Scott/htseq-count-panda/")
 
 import pandas as pd
 
-df = pd.read_table("RNA_1.rawcounts",header=None)
+df = pd.read_table("counts/RNA_1.rawcounts",header=None)
+df.tail
+df2 = df[1:2794]
+sum(df2.loc[:,1])
 
-sum = df.sum(0)
+df3 = df2[df2.loc[:,1]>0]
+df3.sort(1)
+df.tail
+print("I don't think this is the right {} counts").format("raw")
 
+df3.head(5)
+
+for index, row in df3.iterrows():
+    print("This is the name of the gene: {} \
+    and this is the value: {}").format(row[0],row[1])
+    
